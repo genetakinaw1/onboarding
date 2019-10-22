5962af7e897a84a7fc0a21dbaf4ce77d2add7d99
genet.akinaw1 <genet.akinaw1@gmail.com>
2019-10-22 16:28:47 -0500

    number and string excersises

---
+++ week_2/week_2_day_1.rb
@@ -0,0 +1,275 @@
+#pairs1 = [ "code", "bug"]
+#pairs2 = ["man", "moon", "main"]
+#pairs3= ["man", "moon", "good", "night"]
+#final_hash = {}
+#pairs1.each do |var|
+    # final_hash[] = ""    puts var[0] puts var[-1]
+   # final_hash[var[0]] = var[-1]
+#end
+#puts final_hash
+#def squar(number)
+    #number * number
+#end
+#number = 30
+#numbersquar = squar(number)
+#print numbersquar
+#def pairs (arry)
+   
+    
+#def test(a = "ruby",b = "peri")
+   # puts "the program language is (a)"
+    #puts "the progam language is (b)"
+#end
+#test
+#def test 
+#i = 10
+#j = 20
+#k = 30
+
+ #return i,j,k
+#end
+#x = test
+#puts x
+#def sayhi
+    #puts "hello user"
+
+#end
+#sayhi
+# arry = ["code", "bug"]
+# def pairs1(arry)
+
+#     final_hash = Hash.new(0)
+
+#     arry.each do |var|
+#         final_hash[var[0]] = var[-1]
+#     end
+#    final_hash
+# end
+
+
+# puts pairs1(arry)
+
+# arry2 = ["man", "goon", "fain"]
+
+# def pairs2 (arry2)
+#     final_hash = Hash.new(0)
+
+#     arry2.each do |item|
+#     final_hash [item [0]] = item[-1]
+#     end
+#      final_hash
+# end
+
+# puts pairs2(arry2)
+
+#3rd question
+
+# arry3 = ["man", "moon", "good", "night"]
+
+# def pairs3 (arry3)
+#     final_hash = {}
+#     arry3.each do |word|
+#         final_hash [word[0]] = word[-1]
+#     end
+#     final_hash
+# end
+# puts pairs3(arry3)
+#=======Task 2=====
+# arry = [1,2,19]
+# def no9(arry)
+#   arry.delete_if {|number| number % 10 ==9}
+# end
+
+# print no9(arry)
+#*************
+#*******
+
+
+#question 2
+
+# arry2 = [9,19,29,3]
+# def no9(arry2)
+#   arry2.delete_if {|val| val % 10 ==9}
+
+# end
+
+# print  "#{no9(arry2)} \n"
+
+# arry3 = [1,2,9]
+
+# def no9 (arry3)
+#    arry3.delete_if { |val| val % 10 == 9}
+
+# end
+
+# print  no9(arry3)
+#  print "#{no9(arry3)}  \n"
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+# rr1 = [19,21]
+# arr2 = [21,19]
+# arr3 = [19,22]
+# puts arr1.select { |item| item <= 21}.max
+# puts arr2.select {|item| item <= 21}.max
+# puts arr3.select { |item| item <= 21}.max
+
+# task 2 for week 4
+#word_count1 = [ "a","b","a","c","b"]
+#word_count2 = ["c", "b","a"]
+#word_count3  = ["c","c","c","c"]
+
+
+#final_hash = Hash.new(0)
+
+
+#word_count1.each do | item1|
+#final_hash [item1] += 1
+#end
+#puts final_hash
+
+#final_hash = Hash.new(0)
+#word_count3.each do  | item2|
+  #  final_hash [item2] += 1
+#end
+ #puts final_hash
+ # sample exersise
+ 
+#  name ="Boris"
+#  last_name= "Alebachew"
+#  handsome = true
+#  age = 24 + 1
+
+#  puts name
+#  puts last_name
+#  puts age
+#  puts name + " " + last_name
+#  puts handsome
+#  puts age
+#  puts age + 5
+#  age = 50
+#  puts age
+#  name = "my faverite number"
+#  name= 24
+#  puts name
+#  a = 10
+#  b = 20
+#  c = 30
+#  p a
+#  p b
+#  p c
+#  p a,b,c
+# a, b, c = 10,40,445
+# p a, b, c 
+# swaping
+
+# a = 1
+# b = 2
+# p a, b
+#NAME = "genet"
+# PIE = 3.1259
+# p NAME
+# p PIE 
+# p "my name is #{NAME}"
+# name = "genet"
+#p "my name is # {genet}"
+# p "my name is " + name
+# NAME = "Jeff"
+# p NAME
+# p "hello world". length
+# p "hello world".upcase
+# foo = " Hello world"
+# foo.length
+# foo.upcase
+# p 10.next
+# p -1.next 
+# puts "hello world".inspect
+#p "hello world".inspect
+# name = "Alebachew"
+#p "my dad name is #{name}"
+# description = " she is very hardworker and works as a team besides she is determined what is belives"
+# p  "Here is belaynesh biography #{description}!"
+# name = "boies"
+# p "hello #{name}, how are you?"
+# age = 25
+# p " I am " + age.to_s + "years old."
+# p " I am #{age} years old."
+# p "the result of adding 1+1 is #{1+1}"
+# p " in five years I will be #{age + 5}"
+# x = 10
+# y = 15
+# p " the sum of x and y is #{x+y}"
+# name = gets 
+# p "thank you giving you name back. seey "
+# puts " what is you name?"
+# name = gets.chomp
+# puts " Great! what is your age?"
+# age = gets.chomp.to_i
+# puts "cool, your name is #{name} and you are #{age} year old"
+# str = 9999999999999999999999999999
+# p str.class
+
+# p str 
+# p str.class
+# p str.to_i
+# genet = "1356"
+# p genet.to_i
+# p genet.to_i.class
+# p genet.to_f
+# p genet.to_f.class
+# p genet.to_r.class
+# p genet.to_c.class
+# pi = 3.14
+# p pi
+# p pi.to_i
+# p pi.class
+# p pi.to_i.class
+# p 5 < 10
+# p 10 > 15
+# age = 25
+# handsome = true
+# stupid = false 
+
+# p handsome.class 
+# p stupid.class
+# p 11.odd?
+# p 2.even?
+# p 3.even?
+# p "5" != 5
+# p 5 != 5.0
+# p "5" != "5"
+# p "hello" != "goodbye"
+# p "hello".lowercase != "Hello".lowercase
+# p 1 < 5
+# p 1<= 5
+# p 5 <= 5
+# p 20.between?(10,30)
+# p -10.5.between?(-10,20)
+# p 10.6.to_i.class
+# pi = 10.6137
+# p pi.floor
+# p pi.ceil
+# p pi.round
+# p pi.round(3)
+# p pi.round(2)
+# p pi.round(1)
+# p pi.abs
+
+# 2.times do
+
+# puts "you are great!"
+# puts "we are happy to say thankyou!"
+# end
+
+# 10.times {|count| puts "#{3 * (count + 1)}"}
+# 3.times do |var| puts "#{4* (var + 1)}"}
+#.downto(0)  {|count_value| puts " we are alsome! #{count_value}"}
+4.upto(10)  {|count| puts "I am happy! #{count}"}
+
+
+
+
+
+
+
+
